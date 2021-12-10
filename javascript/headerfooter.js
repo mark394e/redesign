857d2ce7f8e65ab80937c8b20cc795257d54ccd9
Victor Ly <vict4222@stud.kea.dk>
2021-12-10 14:04:27 +0100

    tilføjet kommentarer til kode

--- javascript/headerfooter.js
+++ javascript/headerfooter.js
@@ -189,19 +189,17 @@ function pageLoad() {
   // Burgermenu: //
   // // // // // //
 
-  burgerMenu.addEventListener("click", mobileMenu);
+  burgerMenu.addEventListener("click", mobileMenu); //gør burgermenuen klikbart
 
   function mobileMenu() {
-    console.log("clicked");
     burgerMenu.classList.toggle("active");
     navMenu.classList.toggle("active");
-  }
+  } //toggler mellem åbent og lukket
 
   const navLink = document.querySelectorAll(".nav_link");
-
-  navLink.forEach((n) => n.addEventListener("click", closeMenu));
-
+  navLink.forEach((n) => n.addEventListener("click", closeMenu)); //giver hvert links under menuen en eventlistener der lytter på klik, hænger sammen med funktionen nedenunder
   function closeMenu() {
+    //fjerner klassen "active" når man klikker på et link, altså menuen lukker når man går til en anden side
     burgerMenu.classList.remove("active");
     navMenu.classList.remove("active");
   }
